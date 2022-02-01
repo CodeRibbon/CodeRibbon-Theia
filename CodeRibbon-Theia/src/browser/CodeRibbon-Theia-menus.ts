8a88a040b0ac00b5b7b15dc19d5cab11255be2da
Ben Klein <robobenklein@gmail.com>
2022-01-31 19:10:35 -0500

    added toplevel menu item

--- CodeRibbon-Theia/src/browser/CodeRibbon-Theia-menus.ts
+++ CodeRibbon-Theia/src/browser/CodeRibbon-Theia-menus.ts
@@ -2,17 +2,21 @@ import { injectable } from '@theia/core/shared/inversify';
 
 import { CommonMenus } from '@theia/core/lib/browser';
 import {
-  MenuContribution, MenuModelRegistry, MessageService
+  MenuContribution, MenuModelRegistry, MessageService, MenuPath, MAIN_MENU_BAR,
 } from '@theia/core/lib/common';
 
 import { CodeRibbonHelloWorldCommand } from './CodeRibbon-Theia-commands';
+
+export const MenuPath CodeRibbonTopMenuPath = [...MAIN_MENU_BAR, "7_coderibbon"];
 
 @injectable()
 // Add contribution interface to be implemented, e.g. "CodeRibbonTheiaContribution implements CommandContribution"
 export class CodeRibbonTheiaMenuContribution implements MenuContribution {
 
   registerMenus(menus: MenuModelRegistry): void {
-    menus.registerMenuAction(CommonMenus.EDIT, {
+    menus.registerSubmenu(CodeRibbonTopMenuPath, "CodeRibbon");
+
+    menus.registerMenuAction(CodeRibbonTopMenuPath, {
       commandId: CodeRibbonHelloWorldCommand.id,
       label: "Say Hello",
     });
