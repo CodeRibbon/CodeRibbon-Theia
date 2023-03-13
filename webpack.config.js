0205209cf97d5125bf478ebef5ce65e3bc1b544c
Ben Klein <robobenklein@gmail.com>
2023-03-12 21:32:26 -0400

    fix webpack import change

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,12 @@ const config = require('./gen-webpack.config.js');
  * Expose bundled modules on window.theia.moduleName namespace, e.g.
  * window['theia']['@theia/core/lib/common/uri'].
  * Such syntax can be used by external code, for instance, for testing.
-config.module.rules.push({
+config[0].module.rules.push({
     test: /\.js$/,
     loader: require.resolve('@theia/application-manager/lib/expose-loader')
 }); */
 
-config.module.rules.push({
+config[0].module.rules.push({
   test: /\.less/,
   use: [
     'style-loader',
