80bbbb03dc801221d32a79e7f70152bb2f0d0a52
Ben Klein <robobenklein@gmail.com>
2025-02-04 05:12:23 -0500

    fix webpacking of apps

--- browser-app/webpack.config.js
+++ browser-app/webpack.config.js
@@ -3,24 +3,55 @@
  * To reset delete this file and rerun theia build again.
  */
 // @ts-check
-const config = require('./gen-webpack.config.js');
+const configs = require('./gen-webpack.config.js');
+const nodeConfig = require('./gen-webpack.node.config.js');
+const TerserPlugin = require('terser-webpack-plugin');
 
 /**
  * Expose bundled modules on window.theia.moduleName namespace, e.g.
  * window['theia']['@theia/core/lib/common/uri'].
  * Such syntax can be used by external code, for instance, for testing.
-config[0].module.rules.push({
+configs[0].module.rules.push({
     test: /\.js$/,
     loader: require.resolve('@theia/application-manager/lib/expose-loader')
 }); */
 
-config[0].module.rules.push({
+/**
+ * Do no run TerserPlugin with parallel: true
+ * Each spawned node may take the full memory configured via NODE_OPTIONS / --max_old_space_size
+ * In total this may lead to OOM issues
+ */
+if (nodeConfig.config.optimization) {
+    nodeConfig.config.optimization.minimizer = [
+        new TerserPlugin({
+            parallel: false,
+            exclude: /^(lib|builtins)\//,
+            terserOptions: {
+                keep_classnames: /AbortSignal/
+            }
+        })
+    ];
+}
+for (const config of configs) {
+    config.optimization = {
+        minimizer: [
+            new TerserPlugin({
+                parallel: false
+            })
+        ]
+    };
+}
+
+configs[0].module.rules.push({
   test: /\.less/,
   use: [
     'style-loader',
     'css-loader',
     'less-loader'
   ]
-})
+});
 
-module.exports = config;
+module.exports = [
+    ...configs,
+    nodeConfig.config
+];
